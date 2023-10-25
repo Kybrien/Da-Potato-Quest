8ab02f34b61a365e4d0eef3410fdaa397e07c766
BySkullZ <bahrimomo061@gmail.com>
2023-10-25 13:08:10 +0200

    Avancement de map

--- Game/main.cpp
+++ Game/main.cpp
@@ -2,9 +2,23 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "TileMap.h"
+
 #include "GameObject.h"
 #include "Scene.h"
 #include "Menu.h"
+
+void ProcessInput(GameObject* player, float dt)
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		player->SetPosition(player->GetPosition() + Maths::Vector2f(1, 0) * dt * 2);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+		player->SetPosition(player->GetPosition() + Maths::Vector2f(-1, 0) * dt * 2);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+		player->SetPosition(player->GetPosition() + Maths::Vector2f(0, -1) * dt * 2);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		player->SetPosition(player->GetPosition() + Maths::Vector2f(0, 1) * dt * 2);
+}
 
 int main()
 {
@@ -14,31 +28,51 @@ int main()
 
 	GameObject* enemy = scene.CreateDummyGameObject("Enemy", 400.f, "potato.png");
 
-	auto window = new sf::RenderWindow(sf::VideoMode(1920, 1080), "SFML Engine");
+	auto window = new sf::RenderWindow(sf::VideoMode(640, 480), "SFML Engine");
 
-	Menu menu(window);
+	//Menu menu(window);
+	
+	const int level[] =
+	{
+		0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+		1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+		0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+		0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+		0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+		2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+		0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+	};
+
+	TileMap map;
+	if (!map.load("assets/tilesets/Hills.png", sf::Vector2u(16, 16), level, 16, 8))
+		return -1;
+
+	map.loadmap("test");
+
+	sf::Clock clock;
+	sf::Time time;
+
+	float dt = 0;
+	const int speed = 100;
 
 	while (window->isOpen())
 	{
-
 		sf::Event event;
 		while (window->pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window->close();
-			if (event.type == sf::Event::KeyPressed)
-				if (event.key.code == sf::Keyboard::Right || event.key.code == sf::Keyboard::D)
-					player->SetPosition(player->GetPosition() + Maths::Vector2f::Right);
-				if (event.key.code == sf::Keyboard::Left || event.key.code == sf::Keyboard::Q)
-					player->SetPosition(player->GetPosition() + Maths::Vector2f::Left);
-				if (event.key.code == sf::Keyboard::Up || event.key.code == sf::Keyboard::Z)
-					player->SetPosition(player->GetPosition() + Maths::Vector2f::Up);
-				if (event.key.code == sf::Keyboard::Down || event.key.code == sf::Keyboard::S)
-					player->SetPosition(player->GetPosition() + Maths::Vector2f::Down);
 		}
+
+		time = clock.restart();
+		dt = time.asSeconds();
+
+		ProcessInput(player, dt * speed);
 
 		scene.Update();
 		window->clear(sf::Color::Black);
+		window->draw(map);
 		scene.Render(window);
 		window->display();
 	}
